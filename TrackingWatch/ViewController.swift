9fbab19c66bb8af46d6c41e29030e0dc2db635f1
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-02-24 14:06:50 +0500

    Initial Commit

---
+++ TrackingWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackingWatch
+//
+//  Created by Алексей Пархоменко on 24.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
